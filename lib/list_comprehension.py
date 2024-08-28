1dc08b2834d7fdec30536dbf5c9e9ad7810abdb7
mark <kipkoechtony510@gmail.com>
2024-08-29 00:31:03 +0300

    Add list comprehension functions and tests

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
+from typing import List
 
-def return_evens(num_list):
-    pass
+def return_evens(numbers: List[int]) -> List[int]:
+    return [n for n in numbers if n % 2 == 0]
 
-def make_exclamation(sentence_list):
-    pass
+def make_exclamation(sentences: List[str]) -> List[str]:
+    return [sentence + "!" for sentence in sentences]
